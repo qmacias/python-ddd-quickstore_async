c5270dd31fc4b459c04ac43b8475d75b032f2755
qmacias <ezequielqmacias@gmail.com>
2024-10-10 13:57:08 -0300

    Reallocate Common Container Modules

--- src/apps/container.py
+++ src/apps/container.py
@@ -1,63 +1,11 @@
-from injector import Module, singleton, provider, Injector
-from logging import Logger, getLogger, INFO, Formatter, StreamHandler
-
-from settings import settings
+from injector import Injector
 
 from src.apps.backoffice.backend.deps import BackofficeModule
 from src.apps.quickstore.backend.deps import QuickstoreModule
 
-from src.contexts.shared.infrastructure.persistence.MongoConfig import MongoConfig
-from src.contexts.shared.infrastructure.InMemoryEventBus import InMemoryEventBus
-from src.contexts.shared.domain.EventSubscriber import EventSubscriber
-from src.contexts.shared.domain.EventBus import EventBus
-
-
-class LoggerModule(Module):
-    @singleton
-    @provider
-    def logger(self) -> Logger:
-        return config_logger(__name__)
-
-
-def config_logger(name: str) -> Logger:
-    logger = getLogger(name)
-
-    logger.setLevel(INFO)
-
-    formatter = Formatter(
-        '%(levelname)s:%(message)s:%(events)s'
-    )
-
-    stream_handler = StreamHandler()
-    stream_handler.setFormatter(formatter)
-
-    logger.addHandler(stream_handler)
-
-    return logger
-
-
-class EventBusModule(Module):
-    @singleton
-    @provider
-    def event_bus(self) -> EventBus:
-        event_bus = InMemoryEventBus()
-
-        return event_bus
-
-
-def configure_event_bus(
-        event_bus: EventBus, subscribers: list[EventSubscriber],
-) -> None:
-    event_bus.add_subscribers(subscribers)
-
-
-class MongoConfigModule(Module):
-    @singleton
-    @provider
-    def config(self) -> MongoConfig:
-        config = MongoConfig(uri=settings.MONGODB_URI)
-
-        return config
+from src.contexts.shared.infrastructure.dependencies.LoggerModule import LoggerModule
+from src.contexts.shared.infrastructure.dependencies.EventBusModule import EventBusModule
+from src.contexts.shared.infrastructure.dependencies.MongoConfigModule import MongoConfigModule
 
 
 container = Injector(
